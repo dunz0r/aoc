740e4ed7121afe46ba65984af6bdaabc1efacb72
Gabriel Fornaeus <gf@hax0r.se>
2022-11-30 12:00:32 +0100

    A tale of Dragons, Heroes and Legends...

--- aoc_utils.py
+++ aoc_utils.py
@@ -5,14 +5,10 @@
 """
 Share utilities for aoc
 """
-import datetime
 import requests
 import html2text
 import os
 
-curdate=datetime.datetime.now()
-YEAR=curdate.year
-DAY=curdate.day
 DIR=os.environ['AOCDIR']
 PATH=os.environ['AOCDIR']+'./'+str(DAY)
 
@@ -20,8 +16,8 @@ def readInput():
     f = open(PATH+"/input", "r")
     return f.readlines()
 
-def submitAnswer(input,level):
+def submitAnswer(input,year, day, level):
     SESSIONID=os.environ['AOCSESSION']
-    uri = 'http://adventofcode.com/{year}/day/{day}/answer'.format(year=YEAR, day=DAY)
+    uri = 'http://adventofcode.com/{year}/day/{day}/answer'.format(year, day)
     answer = requests.post(uri, cookies={'session': SESSIONID}, data={'answer':str(input), 'level':str(level)})
     return html2text.html2text(answer.text)
