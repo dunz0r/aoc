caa81b7afcf2358e0f140a65f855b086c9afdf65
Gabriel Fornaeus <gf@hax0r.se>
2022-12-01 09:42:25 +0100

    debug suff

--- aoc_utils.py
+++ aoc_utils.py
@@ -9,14 +9,12 @@ import requests
 import html2text
 import os
 
-DIR=os.environ['AOCDIR']
-PATH=os.environ['AOCDIR']+'./'+str(DAY)
 
 def readInput():
-    f = open(PATH+"/input", "r")
+    f = open("input", "r")
     return f.readlines()
 
-def submitAnswer(input,year, day, level):
+def submitAnswer(input, year, day, level):
     SESSIONID=os.environ['AOCSESSION']
     uri = 'http://adventofcode.com/{year}/day/{day}/answer'.format(year, day)
     answer = requests.post(uri, cookies={'session': SESSIONID}, data={'answer':str(input), 'level':str(level)})
