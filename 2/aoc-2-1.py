5cd39902ad658f4141ed6426c0abaa29c4bcaf80
Gabriel Fornaeus <gf@hax0r.se>
2022-12-02 14:24:15 +0100

    Undoing last comming

--- 2/aoc-2-1.py
+++ 2/aoc-2-1.py
@@ -42,7 +42,6 @@ def main():
         line = line.split(" ")
         games.append([translate_move(line[0]), translate_move(line[1])])
     for game in games:
-        winner = scoring_table[game[0]][game[1]]
         if game[1] == 0:
             print("loose")
             if game[0] == 0:
@@ -62,6 +61,7 @@ def main():
                 game[1] = 2
             elif game[0] == 2:
                 game[1] = 0
+        winner = scoring_table[game[0]][game[1]]
         if game[1] == game[0]:
             score = score + 3 + (game[1]+1)
         elif winner == game[1]:
