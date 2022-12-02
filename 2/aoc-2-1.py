3d02e71a6c718f2565416568a6f82a40ec19e60b
Gabriel Fornaeus <gf@hax0r.se>
2022-12-02 14:02:31 +0100

    derp, helper method rename

--- 2/aoc-2-1.py
+++ 2/aoc-2-1.py
@@ -10,10 +10,66 @@ import os
 sys.path.append(os.environ['AOCDIR'])
 from aoc_utils import readInput as readInput, submitAnswer as submitAnswer
 
+"""
+Scoring
+A, X = Rock, 0
+B, Y = Paper, 1
+C, Z = Scissors, 2
+
+Rock > Scissors
+Paper > Rock
+Scissors > Paper
+"""
+
+def translate_move(move):
+    game_map = {
+            'A': 0,
+            'X': 0,
+            'B': 1,
+            'Y': 1,
+            'C': 2,
+            'Z': 2,
+        }
+    return game_map.get(move)
+
 def main():
     input = readInput()
-    result = '.'.join(input)
-    submitAnswer(result,2022,1,1)
+    input = list(map(lambda x:x.strip(),input))
+    scoring_table = [[-1,1,0], [1,-1,2], [0,2,-1]]
+    games = []
+    score = 0
+    for line in input:
+        line = line.split(" ")
+        games.append([translate_move(line[0]), translate_move(line[1])])
+    for game in games:
+        winner = scoring_table[game[0]][game[1]]
+        if game[1] == 0:
+            print("loose")
+            if game[0] == 0:
+                game[1] = 2
+            elif game[0] == 1:
+                game[1] = 0
+            elif game[0] == 2:
+                game[1] = 1
+        elif game[1] == 1:
+            print("draw")
+            game[1] = game[0]
+        elif game[1] == 2:
+            print("win")
+            if game[0] == 0:
+                game[1] = 1
+            elif game[0] == 1:
+                game[1] = 2
+            elif game[0] == 2:
+                game[1] = 0
+        if game[1] == game[0]:
+            score = score + 3 + (game[1]+1)
+        elif winner == game[1]:
+            score = score +6 + (game[1]+1)
+        elif winner == game[0]:
+            score = score + (game[1] + 1)
+        print("Game input: {inp}, score: {p}".format(inp=game, p=score))
+    print(score)
 
 if __name__ == "__main__":
     if not 'AOCSESSION' in os.environ or not 'AOCDIR' in os.environ:
