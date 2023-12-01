985ed428c67f1b4f613b99673f41b6352e943b4c
Gabriel Fornaeus <gf@hax0r.se>
2023-12-01 12:17:10 +0100

    You can't see it, but I'm making a very angry face right now

---
+++ 1/aoc-1-0.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python3
+# vim:fenc=utf-8
+#
+
+"""
+Boilerplate for aoc
+"""
+import sys
+import os
+import re
+sys.path.append(os.environ['AOCDIR'])
+from aoc_utils import readInput as readInput, submitAnswer as submitAnswer
+
+def main():
+    singleNumberSearch = re.compile('[0-9]')
+    twoNumberSearch = re.compile('[0-9]{2,}')
+
+    input = readInput()
+    input = list(map(lambda x:x.strip(),input))
+    numbers = []
+    for line in input:
+        firstHit = str(re.search(singleNumberSearch, line).group())
+        # Reverse the line
+        line = "".join(reversed(line))
+        lastHit = str(re.search(singleNumberSearch, line).group())
+        numbers.append(int(firstHit + lastHit))
+    result = sum(numbers)
+    print(result)
+    #submitAnswer(result,2023,1,1)
+
+if __name__ == "__main__":
+    if not 'AOCSESSION' in os.environ or not 'AOCDIR' in os.environ:
+        print("Set your variables")
+        exit(1)
+    main()
