41e35b2475cc9e5bfe659b31faf790902ce0df84
Gabriel Fornaeus <gf@hax0r.se>
2023-12-07 13:32:12 +0100

    I know, I know, this is not how I’m supposed to do it, but I can't think of something better.

--- 2/aoc-2-0.py
+++ 2/aoc-2-0.py
@@ -13,7 +13,41 @@ from aoc_utils import readInput as readInput, submitAnswer as submitAnswer
 def main():
     input = readInput()
     input = list(map(lambda x:x.strip(),input))
-    print(input)
+    max_red = 12
+    max_green = 13
+    max_blue = 14
+    possible_games = []
+    games = []
+    for game in input:
+        max_counts = {}
+        game_id, pulls = game.split(": ")
+        game_id = game_id.replace('Game ', '')
+        print(game_id)
+        pulls = pulls.split("; ")
+        for pull in pulls:
+            print(pull.split(', '))
+            for cubeset in pull.split(', '):
+                count, colour = cubeset.split(' ')
+                count = int(count)
+                if colour not in max_counts or count > max_counts[colour]:
+                    max_counts[colour] = count
+            games.append({
+                'id': int(game_id),
+                'max_counts': max_counts
+                })
+        for game in input:
+            if game['max_counts'].get('red', 0) > max_red:
+                continue
+            if game['max_counts'].get('blue', 0) > max_blue:
+                continue
+            if game['max_counts'].get('green', 0) > max_green:
+                continue
+            possible_games.append(game['id'])
+
+        print(sum(possible_games))
+
+
+                
     #submitAnswer(result,2022,1,1)
 
 if __name__ == "__main__":
