e5abd62aa2bd35a3b973d2acfdc5953ab563d1fa
Gabriel Fornaeus <gf@hax0r.se>
2023-12-07 10:47:26 +0100

    Improvements

--- 2/aoc-2-0.py
+++ 2/aoc-2-0.py
@@ -13,26 +13,8 @@ from aoc_utils import readInput as readInput, submitAnswer as submitAnswer
 def main():
     input = readInput()
     input = list(map(lambda x:x.strip(),input))
-    l = 0
-    sum = 0
-    red,blue,green = 12,13,14
-    for game in input:
-
-        index,game = game.split(": ")
-        game = game.split(';')
-        print(f'game {index}')
-        red,blue,green = 12,13,14
-        for pull in game[1].split(", "):
-            if 'red' in pull:
-                red -= int(red[0:1])
-            if 'blue' in pull:
-                blue -= int(blue[0:1])
-            if 'green' in pull:
-                green -= int(green[0:1])
-        if not(red>=0 and b=>0 and c >=0):
-            sum += int(index) + 1
-            print(f'game {index} is possible, adding to sum. sum currently: {sum}')
-            print(f' game_red: {game_red}, game_blue: {game_blue}, game_green: {game_green}')
+    print(input)
+    #submitAnswer(result,2022,1,1)
 
 if __name__ == "__main__":
     if not 'AOCSESSION' in os.environ or not 'AOCDIR' in os.environ:
