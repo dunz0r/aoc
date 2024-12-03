5b69d5d2875f1202813e10166f46a27d8aa5e715
Gabriel Fornaeus <gf@hax0r.se>
2024-12-03 15:30:28 +0100

    Don't Ask Me, I Have No Idea Why This Works Either

---
+++ 2/aoc-2-0.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python3
+# vim:fenc=utf-8
+#
+
+"""
+Boilerplate for aoc
+"""
+import sys
+import os
+sys.path.append(os.environ['AOCDIR'])
+from aoc_utils import readInput as readInput, submitAnswer as submitAnswer
+
+def main():
+    input = readInput()
+    input = list(map(lambda x:x.strip(),input))
+    """
+    Levels are safe if any of the conditions below are true:
+        The levels are either all increasing or all decreasing.
+        Any two adjacent levels differ by at least 1 and at most 3.
+    """
+    tally = 0
+    for idx,data in enumerate(input):
+        splData = data.split(' ')
+        splData = [int (x) for x in splData]
+        print(splData)
+        if splData[1] <= (splData[0] + 3):
+            print("Safe")
+        else:
+            print("Unsafe")
+
+if __name__ == "__main__":
+    if not 'AOCSESSION' in os.environ or not 'AOCDIR' in os.environ:
+        print("Set your variables")
+        exit(1)
+    main()
